f1c0feba166a76da9c135ed6073ef8f357f8f016
Alan Jian <alanjian85@outlook.com>
2024-05-20 14:02:13 +0800

    Set binary base address depending on the platform

--- include/platforms/d1/platform.h
+++ include/platforms/d1/platform.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#define BOOT_ENTRY_BASE 0x40200000
 #define BOOT_STACK_BASE 0x80000000
 
 #define PLIC_BASE 0x10000000
@@ -10,7 +11,7 @@
 
 #define TIMER_FREQ 24000000
 
-#if !defined(__ASSEMBLER__)
+#if !defined(__ASSEMBLER__) && !defined(__LINKER__)
 #include <types.h>
 
 i32 init_platform(void);
--- include/platforms/virt/platform.h
+++ include/platforms/virt/platform.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#define BOOT_ENTRY_BASE 0x80200000
 #define BOOT_STACK_BASE 0x88000000
 
 #define PLIC_BASE 0x0C000000
@@ -10,7 +11,7 @@
 
 #define TIMER_FREQ 10000000
 
-#if !defined(__ASSEMBLER__)
+#if !defined(__ASSEMBLER__) && !defined(__LINKER__)
 #include <types.h>
 
 i32 init_platform(void);
