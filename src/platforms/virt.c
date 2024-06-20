3096e0c2f20e8ed07ae4a2b552bfa82009a86a45
Alan Jian <alanjian85@outlook.com>
2024-06-20 10:53:37 +0800

    Remove VirtIO drivers from source tree

--- src/platforms/virt.c
+++ src/platforms/virt.c
@@ -3,14 +3,6 @@
 const dev_node_t DEV_TABLE[] = {
     {.name = "plic", .addr = 0x0C000000},
     {.name = "uart8250", .addr = 0x10000000, .irq = 10, .reg_size = 1},
-    {.name = "virtio", .addr = 0x10001000},
-    {.name = "virtio", .addr = 0x10002000},
-    {.name = "virtio", .addr = 0x10003000},
-    {.name = "virtio", .addr = 0x10004000},
-    {.name = "virtio", .addr = 0x10005000},
-    {.name = "virtio", .addr = 0x10006000},
-    {.name = "virtio", .addr = 0x10007000},
-    {.name = "virtio", .addr = 0x10008000},
     {},
 };
 
